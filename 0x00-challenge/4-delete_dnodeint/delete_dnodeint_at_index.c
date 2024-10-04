46ea44ae2ab8eef94161871afaf22c1e68a27b03
Ebendttl <ebendttl@gmail.com>
2024-10-04 14:55:37 +0100

    Made non-executable

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -19,17 +19,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		return (-1);
 	}
-	if (index == 0)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
-		{
-			tmp->prev = NULL;
-		}
-		return (1);
-	}
 	saved_head = *head;
 	p = 0;
 	while (p < index && *head != NULL)
@@ -42,12 +31,22 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = saved_head;
 		return (-1);
 	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
 	else
 	{
 		(*head)->prev->next = (*head)->next;
+		free(*head);
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
-		free(*head);
 		*head = saved_head;
 	}
 	return (1);
